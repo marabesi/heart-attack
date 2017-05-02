2a307ebd3fe878900d0a1ee059d5bff569edf591
marabesi <matheus.marabesi@gmail.com>
2017-05-01 22:25:48 -0300

    enemy prefab/script added

---
+++ Assets/Scripts/SpawnerManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerManager : MonoBehaviour {
+
+
+	public GameObject enemy;
+	float x;
+	Vector2 whereSpawn;
+	public float spawnTime = 2f;
+	float nextSpawn = 0f;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		if (Time.time > nextSpawn) 
+		{
+			nextSpawn = Time.time + spawnTime;
+			x = Random.Range (362.8f, 428.6f);
+			whereSpawn = new Vector2 (x, transform.position.y);
+			Instantiate (enemy, whereSpawn, Quaternion.identity);
+		}
+	}
+}
